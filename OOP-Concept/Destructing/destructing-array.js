f3a195a7abeb361beb5999e0e143134ef2c1c32e
waliullah <waliullah@gmail.com>
2022-02-04 12:55:46 +0500

    some adding

--- OOP-Concept/Destructing/destructing-array.js
+++ OOP-Concept/Destructing/destructing-array.js
@@ -1,11 +1,11 @@
-console.log('Destructing an Array')
-function destructArray(){
-    const courses = ['C++', 'Java', 'Javascript', 'Python'];
-    return courses;
-}
+console.log("Destructing an Array");
+// function destructArray(){
+const courses = ["C++", "Java", "Javascript", "Python"];
+//     return courses;
+// }
 
 // destructing array
-const [course1, ,course3,course4] = destructArray();
+const [course1, , course3, course4] = courses;
 console.log(course1);
 console.log(course3);
 console.log(course4);
